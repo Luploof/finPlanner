23a2bea429c8f921c920b92bad052e15aee3442a
Julia <luploof@gmail.com>
2026-01-01 17:08:01 +0300

    Initial Commit

---
+++ finPlanner/finPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  finPlannerApp.swift
+//  finPlanner
+//
+//  Created by admin on 1.01.26.
+//
+
+import SwiftUI
+
+@main
+struct finPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ finPlannerTests/finPlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  finPlannerTests.swift
+//  finPlannerTests
+//
+//  Created by admin on 1.01.26.
+//
+
+import Testing
+@testable import finPlanner
+
+struct finPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
